0a8e02986a70f848f89af1c6ac023fb601de65c5
MilenaO02 <miordonezle@uide.edu.ec>
2026-06-25 15:36:59 -0500

    feat: pipeline JWT + rate limiting en index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, {type Request, type Response, type NextFunction} from 'express';
 import { requestLogger } from './middlewares/logger.js';
-import { requireApiKey } from './middlewares/auth.js';
+import { requireJwt } from './middlewares/auth.js';
+import { rateLimiter }  from './middlewares/rateLimiter.js';
 // importar las rutas v1 y v2 para inscripciones
 import v1Inscripciones from './routes/v1/inscripciones.js';
 import v2Inscripciones from './routes/v2/inscripciones.js';
@@ -10,7 +11,8 @@ const app = express();
 //Usando Middleware
 app.use(express.json());
 app.use(requestLogger);
-app.use(requireApiKey);
+app.use(requireJwt);
+app.use(rateLimiter);
 // const no se puede reasignar un valor, es constante
 // let se puede setear el valor, es variable
 // var es para declarar variables globales
